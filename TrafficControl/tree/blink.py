4675ec171411a5556663c30ff5b3a1dc041a5c84
Thomas Denton <tommydenton@gmail.com>
2020-09-07 19:29:58 -0500

    dern gpio pins

--- TrafficControl/tree/blink.py
+++ TrafficControl/tree/blink.py
@@ -5,14 +5,14 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT) #relay 1
-GPIO.setup(23,GPIO.OUT) #relay 2
-GPIO.setup(24,GPIO.OUT) #relay 3
-GPIO.setup(25,GPIO.OUT) #relay 4
-GPIO.setup(12,GPIO.OUT) #relay 5
-GPIO.setup(16,GPIO.OUT) #relay 6
-GPIO.setup(20,GPIO.OUT) #relay 7
-GPIO.setup(21,GPIO.OUT) #relay 8
+GPIO.setup(18,GPIO.OUT) #relay 1 white
+GPIO.setup(23,GPIO.OUT) #relay 2 blue
+GPIO.setup(24,GPIO.OUT) #relay 3 yellow1
+GPIO.setup(25,GPIO.OUT) #relay 4 yellow2
+GPIO.setup(12,GPIO.OUT) #relay 5 yellow3
+GPIO.setup(16,GPIO.OUT) #relay 6 green
+GPIO.setup(20,GPIO.OUT) #relay 7 red
+GPIO.setup(21,GPIO.OUT) #relay 8 open
 
 while True:
 		GPIO.output(18,GPIO.LOW) 
@@ -23,30 +23,31 @@ while True:
 		GPIO.output(16,GPIO.LOW)
 		GPIO.output(20,GPIO.LOW)
 		GPIO.output(21,GPIO.LOW)
-		GPIO.output(12,GPIO.HIGH) #white
-		time.sleep(.5) 
-		GPIO.output(12,GPIO.HIGH) #blue
+		GPIO.output(18,GPIO.HIGH) #white
+		time.sleep(1) 
+		GPIO.output(23,GPIO.HIGH) #blue
 		time.sleep(3) 
-		GPIO.output(12,GPIO.LOW) #blue
-		GPIO.output(16,GPIO.HIGH) #yellow1
+		GPIO.output(18,GPIO.LOW) #white
+		GPIO.output(23,GPIO.LOW) #blue
+		GPIO.output(24,GPIO.HIGH) #yellow1
 		time.sleep(.1)
-		GPIO.output(18,GPIO.HIGH) #yellow2
+		GPIO.output(25,GPIO.HIGH) #yellow2
 		time.sleep(.1)
-		GPIO.output(20,GPIO.HIGH) #yellow3
+		GPIO.output(12,GPIO.HIGH) #yellow3
 		time.sleep(.2)
-		GPIO.output(21,GPIO.HIGH) #green
-		GPIO.output(16,GPIO.LOW) #yellow1
-		GPIO.output(18,GPIO.LOW) #yellow2
-		GPIO.output(20,GPIO.LOW) #yellow3
+		GPIO.output(16,GPIO.HIGH) #green
+		GPIO.output(12,GPIO.LOW) #yellow1
+		GPIO.output(25,GPIO.LOW) #yellow2
+		GPIO.output(24,GPIO.LOW) #yellow3
 		time.sleep(5) 
 		GPIO.output(21,GPIO.LOW) #green
 		time.sleep(3) 
-		GPIO.output(23,GPIO.HIGH) #red
-		GPIO.output(21,GPIO.HIGH) #green
+		GPIO.output(20,GPIO.HIGH) #red
+		GPIO.output(16,GPIO.HIGH) #green
 		time.sleep(1) 
-		GPIO.output(23,GPIO.LOW) #red
-		GPIO.output(21,GPIO.LOW) #green
+		GPIO.output(20,GPIO.LOW) #red
+		GPIO.output(16,GPIO.LOW) #green
 		time.sleep(.1)
-		GPIO.output(23,GPIO.HIGH) #red
-		GPIO.output(21,GPIO.HIGH) #green
+		GPIO.output(20,GPIO.HIGH) #red
+		GPIO.output(16,GPIO.HIGH) #green
 		time.sleep(2)
