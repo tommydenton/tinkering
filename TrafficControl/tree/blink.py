bfb6f41a43546335e0809a4fba846a0d5d66881d
Thomas Denton <tommydenton@gmail.com>
2020-09-10 14:34:43 -0500

    buttons might work

--- TrafficControl/tree/blink.py
+++ TrafficControl/tree/blink.py
@@ -18,16 +18,28 @@ GPIO.setup(24,GPIO.OUT) #relay 8 gate
 GPIO.setup(0,GPIO.OUT) #reset button
 GPIO.setup(2,GPIO.OUT) #start button
 
-def say_hello():
-    print("Hello!")
+def say_hello_reset():
+    print("Hello! reset")
 
-def say_goodbye():
-    print("Goodbye!")
+def say_goodbye_reset():
+    print("Goodbye! reset")
+
+def say_hello_start():
+    print("Hello! start")
+
+def say_goodbye_start():
+    print("Goodbye! start")
 
 button = Button(0)
 
-button.when_pressed = say_hello
-button.when_released = say_goodbye
+button.when_pressed = say_hello_reset
+button.when_released = say_goodbye_reset
+
+button = Button(2)
+
+button.when_pressed = say_hello_start
+button.when_released = say_goodbye_start
+
 
 pause()
 
@@ -36,51 +48,51 @@ pause()
 
 
 while True:
-		GPIO.output(18,GPIO.LOW) 
-		GPIO.output(23,GPIO.LOW) 
-		GPIO.output(21,GPIO.LOW)
-		GPIO.output(25,GPIO.LOW)
-		GPIO.output(12,GPIO.LOW)
-		GPIO.output(16,GPIO.LOW)
-		GPIO.output(20,GPIO.LOW)
-		GPIO.output(24,GPIO.LOW)
-		GPIO.output(18,GPIO.HIGH) #white
-		time.sleep(1) 
-		GPIO.output(23,GPIO.HIGH) #blue
-		time.sleep(3) 
-		GPIO.output(18,GPIO.LOW) #white
-		GPIO.output(23,GPIO.LOW) #blue
-		GPIO.output(21,GPIO.HIGH) #yellow1
-		time.sleep(.1)
-		GPIO.output(25,GPIO.HIGH) #yellow2
-		time.sleep(.1)
-		GPIO.output(12,GPIO.HIGH) #yellow3
-		time.sleep(.2)
-		GPIO.output(16,GPIO.HIGH) #green
-		GPIO.output(12,GPIO.LOW) #yellow1
-		GPIO.output(25,GPIO.LOW) #yellow2
-		GPIO.output(21,GPIO.LOW) #yellow3
-		time.sleep(5) 
-		GPIO.output(16,GPIO.LOW) #green
-		time.sleep(3) 
-		GPIO.output(20,GPIO.HIGH) #red
-		GPIO.output(16,GPIO.HIGH) #green
-		time.sleep(1) 
-		GPIO.output(20,GPIO.LOW) #red
-		GPIO.output(16,GPIO.LOW) #green
-		time.sleep(.5)
-		GPIO.output(20,GPIO.HIGH) #red
-		GPIO.output(16,GPIO.HIGH) #green
-		time.sleep(.5)
-		GPIO.output(20,GPIO.LOW) #red
-		GPIO.output(16,GPIO.LOW) #green
-		time.sleep(.5)
-		GPIO.output(20,GPIO.HIGH) #red
-		GPIO.output(16,GPIO.HIGH) #green
-		time.sleep(.5)
-		GPIO.output(20,GPIO.LOW) #red
-		GPIO.output(16,GPIO.LOW) #green
-		time.sleep(.5)
-		GPIO.output(20,GPIO.HIGH) #red
-		GPIO.output(16,GPIO.HIGH) #green
-		time.sleep(.5)
+	GPIO.output(18,GPIO.LOW) 
+	GPIO.output(23,GPIO.LOW) 
+	GPIO.output(21,GPIO.LOW)
+	GPIO.output(25,GPIO.LOW)
+	GPIO.output(12,GPIO.LOW)
+	GPIO.output(16,GPIO.LOW)
+	GPIO.output(20,GPIO.LOW)
+	GPIO.output(24,GPIO.LOW)
+	GPIO.output(18,GPIO.HIGH) #white
+	time.sleep(1) 
+	GPIO.output(23,GPIO.HIGH) #blue
+	time.sleep(3) 
+	GPIO.output(18,GPIO.LOW) #white
+	GPIO.output(23,GPIO.LOW) #blue
+	GPIO.output(21,GPIO.HIGH) #yellow1
+	time.sleep(.1)
+	GPIO.output(25,GPIO.HIGH) #yellow2
+	time.sleep(.1)
+	GPIO.output(12,GPIO.HIGH) #yellow3
+	time.sleep(.2)
+	GPIO.output(16,GPIO.HIGH) #green
+	GPIO.output(12,GPIO.LOW) #yellow1
+	GPIO.output(25,GPIO.LOW) #yellow2
+	GPIO.output(21,GPIO.LOW) #yellow3
+	time.sleep(5) 
+	GPIO.output(16,GPIO.LOW) #green
+	time.sleep(3) 
+	GPIO.output(20,GPIO.HIGH) #red
+	GPIO.output(16,GPIO.HIGH) #green
+	time.sleep(1) 
+	GPIO.output(20,GPIO.LOW) #red
+	GPIO.output(16,GPIO.LOW) #green
+	time.sleep(.5)
+	GPIO.output(20,GPIO.HIGH) #red
+	GPIO.output(16,GPIO.HIGH) #green
+	time.sleep(.5)
+	GPIO.output(20,GPIO.LOW) #red
+	GPIO.output(16,GPIO.LOW) #green
+	time.sleep(.5)
+	GPIO.output(20,GPIO.HIGH) #red
+	GPIO.output(16,GPIO.HIGH) #green
+	time.sleep(.5)
+	GPIO.output(20,GPIO.LOW) #red
+	GPIO.output(16,GPIO.LOW) #green
+	time.sleep(.5)
+	GPIO.output(20,GPIO.HIGH) #red
+	GPIO.output(16,GPIO.HIGH) #green
+	time.sleep(.5)
