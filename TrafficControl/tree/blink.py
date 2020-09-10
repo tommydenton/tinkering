7fa4b2d37091f2257b62ea62810638ef79a23e78
Thomas Denton <tommydenton@gmail.com>
2020-09-10 18:29:16 -0500

    indents are busted

--- TrafficControl/tree/blink.py
+++ TrafficControl/tree/blink.py
@@ -63,16 +63,16 @@ def start_led():
 
 try:
 	while True:
-	input_state = GPIO.input(resetbtn)
-        if input_state == False:
-		print('Reset Button Pressed')
-		start_led()
-		time.sleep(0.2)
+		input_state = GPIO.input(resetbtn)
+       		 if input_state == False:
+			print('Reset Button Pressed')
+			start_led()
+			time.sleep(0.2)
 
-	input_state = GPIO.input(startbtn)
-        if input_state == False:
-		print('Start Button Pressed')
-		start_led()
-		time.sleep(0.2)
+		input_state = GPIO.input(startbtn)
+        	if input_state == False:
+			print('Start Button Pressed')
+			start_led()
+			time.sleep(0.2)
 except:
 	GPIO.cleanup() 
